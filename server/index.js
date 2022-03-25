1c9b7d3895a6cdef7b861d986e008b34b8ea17fa
Koshana <it18037920@my.sliit.lk>
2022-03-25 23:11:02 +0530

    create post model,routes and controllers

--- server/index.js
+++ server/index.js
@@ -4,12 +4,16 @@ import bodyParser from "body-parser";
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
 app.use(bodyParser.json({ limit : "30mb", extended : true }));
 app.use(bodyParser.urlencoded({ limit : "30mb", extended : true }));
 app.use(cors());
 dotenv.config();
+
+app.use('/posts',postRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
